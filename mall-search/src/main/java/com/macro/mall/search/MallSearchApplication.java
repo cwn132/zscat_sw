b47c647e24d3c25ea7782a34a9614d47934951fa
shenzhuan <zhuan.shen@rjfittime.com>
2019-04-04 21:45:43 +0800

    springboot vue 前后端分离商城

---
+++ mall-search/src/main/java/com/macro/mall/search/MallSearchApplication.java
@@ -0,0 +1,14 @@
+package com.macro.mall.search;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan({"com.macro.mall.mapper", "com.macro.mall.search.dao"})
+public class MallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSearchApplication.class, args);
+    }
+}
